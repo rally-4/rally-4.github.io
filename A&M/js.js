f2860fcffa3ce5ee0174d5a38e9f3df16e910813
rally4 <96571771+rally-4@users.noreply.github.com>
2024-04-19 20:13:08 +0200

    Update js.js

--- A&M/js.js
+++ A&M/js.js
@@ -51,16 +51,16 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const T4 = document.getElementById('T4');
     let TSArr = [TS1, TS2, TS3, TS4];
     let TArr = [T1, T2, T3, T4];
-    obj.res.add = function(n, a, id, col){
+    obj.res.add = function(n, a, col){
         for(i=0; i<n.length; i++){
             if(obj.res[n[i]] == undefined){
                 obj.res[n[i]] = 0;
                 
                 var newCraft = document.createElement('div');
                 TS2.appendChild(newCraft);
-                newCraft.id = 'C' + id[i];
+                newCraft.id = 'C' + n[i];
                 newCraft.classList.add('crafts');
-                newCraft.style.top = 'calc(16px + ' + (id[i]-1)*8 + '%)';
+                newCraft.style.top = 'calc(16px + ' + (obj.res.length-1)*8 + '%)';
                 
                 var newDiv = document.createElement('div');
                 newCraft.appendChild(newDiv);
@@ -73,11 +73,11 @@ window.addEventListener('DOMContentLoaded', ()=>{
                 
                 var p2 = document.createElement('p');
                 newCraft.appendChild(p2);
-                p2.id = 'C' + id[i] + 'C';
+                p2.id = 'C' + n[i] + 'C';
                 p2.innerHTML = 'Quantity: ' + obj.res[n[i]];
             }
             obj.res[n[i]] += a[i];
-            document.getElementById('C' + id[i] + 'C').innerHTML = 'Quantity: ' + obj.res[n[i]];
+            document.getElementById('C' + n[i] + 'C').innerHTML = 'Quantity: ' + obj.res[n[i]];
         }
     }
     
@@ -189,7 +189,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
     var A1Wins = 0; const A1BossWins = 4;
     
     // E -> Enemy, O -> Opponent, U -> User, P -> Player, H -> Health, S -> Shield
-    let LoadFight = function(EnemyName, MaxEH, MaxES, EDowntime, ESR, MaxUH, MaxUS, UDowntime, USR, OpponentAttacks, mi, area, resnarr=[], resarr=[], idarr=[], colarr=[]){
+    let LoadFight = function(EnemyName, MaxEH, MaxES, EDowntime, ESR, MaxUH, MaxUS, UDowntime, USR, OpponentAttacks, mi, area, resnarr=[], resarr=[], colarr=[]){
         let Enemy = document.getElementById('Enemy'); Enemy.innerHTML = 'Incoming: ' + EnemyName + '!';
         Enemy.style.animation = '0.2s ease forwards slide';
         let EnemyN = document.getElementById('EnemyName'); EnemyN.innerHTML = EnemyName;
@@ -459,7 +459,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
                 
                 if(COH <= 0){
                     obj.res.materials += mi;
-                    obj.res.add(resnarr, resarr, idarr, colarr);
+                    obj.res.add(resnarr, resarr, colarr);
                     C1C.innerHTML = 'Quantity: ' + obj.res.materials;
                     
                     if(area == 'A1'){A1Wins++}
@@ -497,12 +497,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
     E1.addEventListener('click', ()=>{
         if(A1Wins < A1BossWins){
             if(Math.random() < .5){
-                LoadFight('Basic Slime', 400, 180, 5, .5, UserHealth, UserShield, ShieldDowntime, ShieldRegen, BasicSlimeAttacks, 1, 'A1', ['Slime'], [Math.round(2*Math.random())+2], [2], ['rgb(0, 127, 255)']);
+                LoadFight('Basic Slime', 400, 180, 5, .5, UserHealth, UserShield, ShieldDowntime, ShieldRegen, BasicSlimeAttacks, 1, 'A1', ['Slime'], [Math.round(2*Math.random())+2], ['rgb(0, 127, 255)']);
             }else{
-                LoadFight('Scrap Slime', 350, 570, 10, .1, UserHealth, UserShield, ShieldDowntime, ShieldRegen, ScrapSlimeAttacks, 1, 'A1', ['Slime'], [Math.round(2*Math.random())+2], [2], ['rgb(0, 127, 255)']);
+                LoadFight('Scrap Slime', 350, 570, 10, .1, UserHealth, UserShield, ShieldDowntime, ShieldRegen, ScrapSlimeAttacks, 1, 'A1', ['Slime'], [Math.round(2*Math.random())+2], ['rgb(0, 127, 255)']);
             }
         }else{
-            LoadFight('Reinforced Slime', 1000, 750, 15, .2, UserHealth, UserShield, ShieldDowntime, ShieldRegen, ReinforcedSlimeAttacks, 5, 'A1B', ['Slime', 'Slime Key'], [Math.round(3*Math.random())+3, 1], [2, 3], ['rgb(0, 127, 255)', 'rgb(0, 90, 210)']);
+            LoadFight('Reinforced Slime', 1000, 750, 15, .2, UserHealth, UserShield, ShieldDowntime, ShieldRegen, ReinforcedSlimeAttacks, 5, 'A1B', ['Slime', 'Slime Key'], [Math.round(3*Math.random())+3, 1], ['rgb(0, 127, 255)', 'rgb(0, 90, 210)']);
         }
     });
 });
