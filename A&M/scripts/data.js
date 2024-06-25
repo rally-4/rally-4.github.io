cebb2f1a5d068975a3169588200d9eec0214b455
rally4 <96571771+rally-4@users.noreply.github.com>
2024-06-25 21:53:41 +0200

    Update data.js

--- A&M/scripts/data.js
+++ A&M/scripts/data.js
@@ -2,13 +2,6 @@ const loadData = function(){
     const C1C = document.getElementById('C1C');
     obj = {res: {materials: 0}, tutorial: true, sBossProgress: {SB1w: false, SB2w: false}, areaProgress: {A1C: false, A2C: false}, areaWins: {A1Wins: 0, A2Wins: 0}, unlocks: {uu2: false, uu3: false, uu6: false, uu7: false, uu8: false}, upgrades: {uu1: 0, uu5: 0}, muls: {ndm: 1, sdm: 1, pdm: 1}}
     
-    // DEBUGGING
-    obj.res.materials += 1000;
-    obj.tutorial = false;
-    obj.areaProgress.A1C = true;
-    obj.areaProgress.A2C = true;
-    recoverAreaUnlocks(obj.areaProgress, obj.areaWins);
-    
     if(sessionStorage.getItem('A&MData')){
         obj = JSON.parse(sessionStorage.getItem('A&MData'));
         recoverAreaUnlocks(obj.areaProgress, obj.areaWins);
@@ -82,6 +75,4 @@ const loadData = function(){
             document.getElementById('C'+n[i]+'C').innerHTML = 'Quantity: ' + obj.res[n[i]];
         }
     }
-    
-    obj.res.add(['Slime','Slime Key', 'Red Fluid'], [120,1,200]); // DEBUGGING
 }
